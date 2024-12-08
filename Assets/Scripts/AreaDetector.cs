50b3c8ceed3664f0fc326bec825f30ed0879d6c7
ivo_hr <81875802+ivo-hr@users.noreply.github.com>
2024-12-08 12:47:55 +0100

    changeds

--- Assets/Scripts/AreaDetector.cs
+++ Assets/Scripts/AreaDetector.cs
@@ -21,6 +21,8 @@ public class AreaDetector : MonoBehaviour
     private FMOD.Studio.EventInstance stepInstance;
 
     [SerializeField]
+    private string currentTile = "None";
+    [SerializeField]
     private string currentArea = "None";
 
     int terrainLayer;
@@ -33,12 +35,12 @@ public class AreaDetector : MonoBehaviour
         stepInstance.set3DAttributes(attributes);
         areaInstance.set3DAttributes(attributes);
 
-        stepInstance.setParameterByName("Volume", stepVolume);
-
         areaInstance = RuntimeManager.CreateInstance(areaSound);
         stepInstance = RuntimeManager.CreateInstance(stepSound);
         areaInstance.start();
         stepInstance.start();
+
+        stepInstance.setParameterByName("Volume", stepVolume);
 
         terrainLayer = LayerMask.GetMask("Terrain");
 
@@ -109,6 +111,7 @@ public class AreaDetector : MonoBehaviour
             if (!ismoving) {
                 ismoving = true;
                 stepInstance.start();
+
             }
         }
         else
@@ -127,7 +130,7 @@ public class AreaDetector : MonoBehaviour
     void NearAreaSensor()
     {
         //Translate the current area to FMOD parameters
-        string currentAreaCode = AreaTranslate(currentArea);
+        currentArea = AreaTranslate(currentTile);
 
 
         //Detect the areas near the player
@@ -142,66 +145,94 @@ public class AreaDetector : MonoBehaviour
 
 
 
-            if (areaCode != currentAreaCode){
+            if (areaCode != currentTile){
 
-
+                //Distance between player and area hit
+                float distance = UnityEngine.Vector3.Distance(player.position, hit.gameObject.transform.position);
 
                 if (nearestAreas.ContainsKey(areaCode))
                 {
                     //Check if the new area is closer than the one in the dictionary
-                    if (UnityEngine.Vector3.Distance(player.position, hit.transform.position) < UnityEngine.Vector3.Distance(player.position, nearestAreas[areaCode].transform.position))
+                    if (distance < UnityEngine.Vector3.Distance(player.position, nearestAreas[areaCode].transform.position))
                     {
                         nearestAreas[areaCode] = hit.gameObject;
                     }
                 }
-                else
+                else if (distance <= maxDistance)
                 {
                     nearestAreas.Add(areaCode, hit.gameObject);
                 }
             }   
         }
 
-        
+
 
         //Check if the nearest area is occluded
         foreach (KeyValuePair<string, GameObject> area in nearestAreas)
         {
-
+            //Get real distance between player and area
+            float distance = UnityEngine.Vector3.Distance(player.position, area.Value.transform.position);
             //Set volume, Direction and Extent parameters
-            areaInstance.setParameterByName(area.Key + "Volume", maxDistance - UnityEngine.Vector3.Distance(player.position, area.Value.transform.position) - ambientVolumeReduction);
+            areaInstance.setParameterByName(area.Key + "Volume", maxDistance - distance - ambientVolumeReduction);
             areaInstance.setParameterByName(area.Key + "Direction", yawAngle(player.position, area.Value.transform.position) + 180);
-            areaInstance.setParameterByName(area.Key + "Distance", UnityEngine.Vector3.Distance(player.position, area.Value.transform.position) * maxDistance / 10000);
+            areaInstance.setParameterByName(area.Key + "Distance", distance * maxDistance / 10000);
             
+            //Get the top center point of the tile
+            UnityEngine.Vector3 topCenter = GetTopCenterPoint(area.Value);
+
+            //Get the point at eye level of the tile relative to the player
+            UnityEngine.Vector3 pointAtPlayerHeight = new UnityEngine.Vector3(area.Value.transform.position.x, 
+                                                                                player.position.y + player.up.y * 1, 
+                                                                                area.Value.transform.position.z
+                                                                                );
+            //Get distance between player and point at player height
+            float distanceToPlayerHeight = UnityEngine.Vector3.Distance(player.position + player.up * 1, pointAtPlayerHeight);
+
             
             RaycastHit hit;
-            if (Physics.Raycast(player.position + player.up * 1, area.Value.transform.position - (player.position + player.up * 1), out hit, maxDistance, terrainLayer))
+            if (Physics.Raycast(player.position + player.up * 1, pointAtPlayerHeight - (player.position + player.up * 1), out hit, distanceToPlayerHeight, terrainLayer))
             {
-                Debug.DrawRay(player.position + player.up * 1, (area.Value.transform.position - (player.position + player.up * 1)).normalized * maxDistance, Color.red, 10f);
+                
 
+                //What the ray hits
+                string rayHitArea = AreaTranslate(hit.collider.tag);
 
-
-                string areaCode = AreaTranslate(area.Value.tag);
+                
 
                 //If the ray hits the same area, no occlusion
-                if (areaCode == area.Key)
+                if (rayHitArea == area.Key)
                 {
                     areaInstance.setParameterByName(area.Key + "Occlusion", 0);
+
+                    Debug.Log("Not Occluded " + area.Key);
+                    Debug.DrawRay(player.position + player.up * 1, ( pointAtPlayerHeight - (player.position + player.up * 1)).normalized * distanceToPlayerHeight, Color.blue, 10f);
                 }
+                //If the ray hits a different area, occlusion!
                 else
+                {
                     areaInstance.setParameterByName(area.Key + "Occlusion", occlusionIntensity); 
 
-                    Debug.Log("Occluded " + area.Key);       
+                    Debug.Log("Occluded " + area.Key + " by " + rayHitArea);   
+                    Debug.DrawRay(player.position + player.up * 1, ( pointAtPlayerHeight - (player.position + player.up * 1)).normalized * distanceToPlayerHeight, Color.red, 10f);
+                }
+    
                 
+            }
+            //If the ray doesn't hit anything, no occlusion! 
+            else
+            {
+                Debug.DrawRay(player.position + player.up * 1, ( pointAtPlayerHeight - (player.position + player.up * 1)).normalized * distanceToPlayerHeight, Color.green, 10f);
+                areaInstance.setParameterByName(area.Key + "Occlusion", 0);
             }
 
         }
 
         //Play the current area sound were the player is
 
-        areaInstance.setParameterByName(currentAreaCode + "Volume", 100 - ambientVolumeReduction);
-        areaInstance.setParameterByName(currentAreaCode + "Direction", 0);
-        areaInstance.setParameterByName(currentAreaCode + "Distance", 0);
-        areaInstance.setParameterByName(currentAreaCode + "Occlusion", 0);
+        areaInstance.setParameterByName(currentArea + "Volume", 100 - ambientVolumeReduction);
+        areaInstance.setParameterByName(currentArea + "Direction", 0);
+        areaInstance.setParameterByName(currentArea + "Distance", 0);
+        areaInstance.setParameterByName(currentArea + "Occlusion", 0);
 
 
         areaInstance.set3DAttributes(attributes);
@@ -227,12 +258,31 @@ public class AreaDetector : MonoBehaviour
     }
 
 
-    float yawAngle(UnityEngine.Vector3 playerPos, UnityEngine.Vector3 areaPos)
+    public static float yawAngle(UnityEngine.Vector3 playerPos, UnityEngine.Vector3 areaPos)
     {
         UnityEngine.Vector3 direction = areaPos - playerPos;
         return Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
     }
+
+
+
+    public static UnityEngine.Vector3 GetTopCenterPoint(GameObject cube)
+    {
+        // Get the collider of the GameObject
+        Collider cubeCollider = cube.GetComponent<Collider>();
+        
+        if (cubeCollider != null)
+        {
+            // Calculate the top-center point
+            UnityEngine.Vector3 topCenter = cubeCollider.bounds.center + UnityEngine.Vector3.up * cubeCollider.bounds.extents.y;
+            return topCenter;
+        }
+        
+        Debug.LogWarning("No collider found on the provided GameObject.");
+        return UnityEngine.Vector3.zero; // Return zero vector if no collider is found
+    }
 }
+
 
 
 
